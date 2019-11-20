23cc9e90b6297370d41b108b488bfed27324da36
Vishal <vishalpatel151196@gmail.com>
2019-11-20 18:52:22 -0500

    Initial Commit

---
+++ SimpleTask-Maria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTask-Maria
+//
+//  Created by Student on 2019-11-20.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
