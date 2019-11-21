46cbe47d7b10ee8c91eb813b3962e6ce8d1560eb
Vishal <vishalpatel151196@gmail.com>
2019-11-20 21:29:47 -0500

    commit1

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+target 'SimpleTask-Maria' do
+  use_frameworks!
+
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Firestore'
+end
--- SimpleTask-Maria/ViewController.swift
+++ SimpleTask-Maria/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
     }
-
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func createu(_ sender: UIButton) {
+        let emailid = email.text
+        let pswd = password.text
+        
+        Auth.auth().createUser(withEmail: emailid!, password: pswd!) { authResult, error in
+            // ...
+        }
+    }
+    
 
 }
-
